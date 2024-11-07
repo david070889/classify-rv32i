c7d3ee1279cc67ced26b14d62ee7e8b6cd86a3e4
david <david070889@gmail.com>
2024-11-07 21:31:00 +0800

    finish relu

--- src/relu.s
+++ src/relu.s
@@ -29,6 +29,21 @@ relu:
 
 loop_start:
     # TODO: Add your own implementation
+    bgt t0, a1, finish
+    lw t2, 0(a0)
+    blt t2, t1, LTZ
+    addi t0, t0, 1
+    addi a0, a0, 4
+
+LTZ:
+    li t2, 0
+    sw t2, 0(a0)
+    addi t0, t0, 1
+    addi a0, a0, 4
+    j loop_start
+
+finish:
+    ret
 
 error:
     li a0, 36          
